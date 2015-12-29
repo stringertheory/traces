a5dd334d83f508e16db903d82e1a452590020c8c
stringertheory <mike.stringer@datascopeanalytics.com>
2015-12-29 16:05:04 -0600

    Fixed setup.py file and bumped version on pypi

--- setup.py
+++ setup.py
@@ -1,41 +1,71 @@
 import os
 import setuptools
 
-import pypandoc
+GITHUB_URL = 'https://github.com/datascopeanalytics/traces'
 
-with open(os.path.join("traces", "__init__.py")) as stream:
-    for line in stream:
-        if "VERSION" in line:
-            version = line.split('=')[-1].strip().replace("'","")
+try:
+    import pypandoc
+except ImportError:
+    pypandoc = None
 
-# read in the description from README and convert to RST for pypi
-with open("README.md") as stream:
-    md = stream.read()
-    long_description = pypandoc.convert(md, 'rst', format='markdown_github')
-    
-github_url='https://github.com/datascopeanalytics/traces'
 
-# read in the dependencies from the virtualenv requirements file
-dependencies = []
-with open('requirements/python.txt', 'r') as stream:
-    for line in stream:
-        package = line.strip().split('#')[0]
-        if package:
-            dependencies.append(package)
+def read_version():
+    """Parse the package __init__ file to find the version so that it's
+    not in multiple places.
+
+    """
+    filename = os.path.join("traces", "__init__.py")
+    version = None
+    with open(filename) as stream:
+        for line in stream:
+            if "VERSION" in line:
+                version = line.split('=')[-1].strip().replace("'", "")
+
+    # throw error if version isn't in __init__ file
+    if version is None:
+        raise ValueError('must define VERSION in %s' % filename)
+                
+    return version
+
+def read_description():
+    """Read in the description from README and convert to RST for pypi if
+    the pypandoc package is available.
+
+    """
+    with open("README.md") as stream:
+        md = stream.read()
+        if pypandoc:
+            long_description = \
+                pypandoc.convert(md, 'rst', format='markdown_github')
+        else:
+            long_description = md
+
+    return long_description
+
+def read_dependencies():
+    """Read in the dependencies from the virtualenv requirements file.
+
+    """
+    dependencies = []
+    with open('requirements/python.txt', 'r') as stream:
+        for line in stream:
+            package = line.strip().split('#')[0].strip()
+            if package:
+                dependencies.append(package)
 
 setuptools.setup(
     name='traces',
-    version=version,
+    version=read_version(),
     description="Tools for analysis of unevenly space time series.",
-    long_description=long_description,
-    url=github_url,
-    download_url="%s/archives/master" % github_url,
+    long_description=read_description(),
+    url=GITHUB_URL,
+    download_url="%s/archives/master" % GITHUB_URL,
     author='Mike Stringer',
     author_email='mike.stringer@datascopeanalytics.com',
     license='MIT',
     packages=[
         'traces',
     ],
-    install_requires=dependencies,
+    install_requires=read_dependencies(),
     zip_safe=False,
 )
