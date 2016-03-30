a0a5ef9576da551b094d22e722c818cc48b31a9f
stringertheory <mike.stringer@datascopeanalytics.com>
2016-03-29 22:22:12 -0500

    Added test for iter_merge returning the same values as merge

--- tests/test_iterators.py
+++ tests/test_iterators.py
@@ -1,4 +1,7 @@
 import datetime
+import random
+import sys
+import pprint
 
 import nose
 
@@ -48,3 +51,32 @@ def test_slice():
     assert ts.slice(1.0, 2.5).items() == [(1.0, 5), (2.5, 5)]
     assert ts.slice(-1, 1).items() == [(-1, 0), (0, 1), (1, 5)]
     assert ts.slice(-1, 0.5).items() == [(-1, 0), (0, 1), (0.5, 1)]
+
+def make_random_timeseries():
+    length = random.randint(0, 10)
+    result = TimeSeries()
+    t = 0
+    for i in range(length):
+        t += random.randint(0, 5)
+        x = random.randint(0, 5)
+        result[t] = x
+    return result
+    
+def test_merge():
+
+    # since this is random, do it a bunch of times
+    for n_trial in range(1000):
+
+        # make a list of TimeSeries that is anywhere from 0 to 5
+        # long. Each TimeSeries is of random length between 0 and 20,
+        # with random time points and random values.
+        ts_list = []
+        for i in range(random.randint(0, 5)):
+            ts_list.append(make_random_timeseries())
+
+        method_a = list(TimeSeries.merge(ts_list))
+        method_b = list(TimeSeries.iter_merge(ts_list))
+        
+        msg = '%s != %s' % (pprint.pformat(method_a), pprint.pformat(method_b))
+        assert method_a == method_b, msg
+
