025ac901a87538e77c95cf246b71d81e5f2c1001
ypleong <ypleong@datascopeanalytics.com>
2016-08-04 13:44:02 -0500

    fix some code for pep8

--- traces/__init__.py
+++ traces/__init__.py
@@ -6,4 +6,3 @@ from .timeseries import TimeSeries
 __author__ = 'Mike Stringer'
 __email__ = 'mike.stringer@datascopeanalytics.com'
 __version__ = '0.0.7'
-
--- traces/cli.py
+++ traces/cli.py
@@ -6,8 +6,7 @@ import click
 @click.command()
 def main(args=None):
     """Console script for traces"""
-    click.echo("Replace this message by putting your code into "
-                "traces.cli.main")
+    click.echo("Replace this message by putting your code into traces.cli.main")
     click.echo("See click documentation at http://click.pocoo.org/")
 
 
