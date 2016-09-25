d3986e48be497a00960a82bcd8ea4662173eda3f
stringertheory <mike.stringer@datascopeanalytics.com>
2016-09-25 13:37:22 -0500

    changed to Beta development status :)

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
     zip_safe=False,
     keywords='traces',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
