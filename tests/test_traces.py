52ef67d9a43297465f1243f5b647f93db959c5e0
Sébastien de Menten <sdementen@gmail.com>
2017-04-15 16:35:43 +0200

    add test for sample_interval

--- tests/test_traces.py
+++ tests/test_traces.py
@@ -1,7 +1,8 @@
 from datetime import datetime, timedelta
 
 import nose
-from traces import Histogram, TimeSeries
+
+from traces import TimeSeries, Domain
 
 
 def test_init_data():
@@ -59,7 +60,7 @@ def test_merge():
     assert True in ts_merge[0]
     assert None in ts_merge[0]
 
-    
+
 def test_set_interval():
     ts = TimeSeries()
     nose.tools.assert_raises(KeyError, ts.get, 0)
@@ -99,6 +100,7 @@ def test_set_interval_datetime():
                           (datetime(2012, 1, 8, 0, 0), 100),
                           (datetime(2012, 1, 10, 0, 0), 10)]
 
+
 def test_remove_points_from_interval():
     ts = TimeSeries(default=0)
     ts[0] = 0
@@ -111,7 +113,7 @@ def test_remove_points_from_interval():
     del ts[3.5:4.5]
 
     assert ts[5] == 1
-    
+
     ts[4] = 0
 
     del ts[3:4.5]
@@ -124,3 +126,89 @@ def test_remove_points_from_interval():
     del ts[3.5:4]
 
     assert ts[5] == 0
+
+
+def test_sample_interval_days():
+    import pandas as pd
+    ts = Domain([(datetime(2012, 1, 1), 400),
+                 (datetime(2012, 3, 1), 400)])
+
+    ts[datetime(2012, 1, 4):datetime(2012, 1, 20)] = 10
+    ts[datetime(2012, 1, 25):datetime(2012, 2, 7)] = 50
+    ts[datetime(2012, 1, 19):datetime(2012, 1, 27)] = 0
+
+    sr = ts.sample_interval(sampling_period=timedelta(days=1), end=datetime(2012, 2, 1))
+    assert list(sr.iteritems()) == [(pd.Timestamp('2012-01-01 00:00:00'), 400.0),
+                                    (pd.Timestamp('2012-01-02 00:00:00'), 400.0),
+                                    (pd.Timestamp('2012-01-03 00:00:00'), 400.0),
+                                    (pd.Timestamp('2012-01-04 00:00:00'), 10.0),
+                                    (pd.Timestamp('2012-01-05 00:00:00'), 10.0),
+                                    (pd.Timestamp('2012-01-06 00:00:00'), 10.0),
+                                    (pd.Timestamp('2012-01-07 00:00:00'), 10.0),
+                                    (pd.Timestamp('2012-01-08 00:00:00'), 10.0),
+                                    (pd.Timestamp('2012-01-09 00:00:00'), 10.0),
+                                    (pd.Timestamp('2012-01-10 00:00:00'), 10.0),
+                                    (pd.Timestamp('2012-01-11 00:00:00'), 10.0),
+                                    (pd.Timestamp('2012-01-12 00:00:00'), 10.0),
+                                    (pd.Timestamp('2012-01-13 00:00:00'), 10.0),
+                                    (pd.Timestamp('2012-01-14 00:00:00'), 10.0),
+                                    (pd.Timestamp('2012-01-15 00:00:00'), 10.0),
+                                    (pd.Timestamp('2012-01-16 00:00:00'), 10.0),
+                                    (pd.Timestamp('2012-01-17 00:00:00'), 10.0),
+                                    (pd.Timestamp('2012-01-18 00:00:00'), 10.0),
+                                    (pd.Timestamp('2012-01-19 00:00:00'), 0.0),
+                                    (pd.Timestamp('2012-01-20 00:00:00'), 0.0),
+                                    (pd.Timestamp('2012-01-21 00:00:00'), 0.0),
+                                    (pd.Timestamp('2012-01-22 00:00:00'), 0.0),
+                                    (pd.Timestamp('2012-01-23 00:00:00'), 0.0),
+                                    (pd.Timestamp('2012-01-24 00:00:00'), 0.0),
+                                    (pd.Timestamp('2012-01-25 00:00:00'), 0.0),
+                                    (pd.Timestamp('2012-01-26 00:00:00'), 0.0),
+                                    (pd.Timestamp('2012-01-27 00:00:00'), 50.0),
+                                    (pd.Timestamp('2012-01-28 00:00:00'), 50.0),
+                                    (pd.Timestamp('2012-01-29 00:00:00'), 50.0),
+                                    (pd.Timestamp('2012-01-30 00:00:00'), 50.0),
+                                    (pd.Timestamp('2012-01-31 00:00:00'), 50.0)]
+
+
+def test_sample_interval_hours():
+    import pandas as pd
+
+    ts = Domain([(datetime(2012, 1, 1), 400),
+                 (datetime(2012, 1, 10), 400)])
+
+    ts[datetime(2012, 1, 4, 12):datetime(2012, 1, 6, 20)] = 10
+    ts[datetime(2012, 1, 7, 9):datetime(2012, 1, 10)] = 50
+
+    sr = ts.sample_interval(sampling_period=timedelta(days=1))
+    assert list(sr.iteritems()) == [(pd.Timestamp('2012-01-01 00:00:00', offset='D'), 400.0),
+                                    (pd.Timestamp('2012-01-02 00:00:00', offset='D'), 400.0),
+                                    (pd.Timestamp('2012-01-03 00:00:00', offset='D'), 400.0),
+                                    (pd.Timestamp('2012-01-04 00:00:00', offset='D'), 205.0),
+                                    (pd.Timestamp('2012-01-05 00:00:00', offset='D'), 10.0),
+                                    (pd.Timestamp('2012-01-06 00:00:00', offset='D'), 75.0),
+                                    (pd.Timestamp('2012-01-07 00:00:00', offset='D'), 181.25),
+                                    (pd.Timestamp('2012-01-08 00:00:00', offset='D'), 50.0),
+                                    (pd.Timestamp('2012-01-09 00:00:00', offset='D'), 50.0)]
+
+    sr = ts.sample_interval(sampling_period=timedelta(days=1), operation="max")
+    assert list(sr.iteritems()) == [(pd.Timestamp('2012-01-01 00:00:00', offset='D'), 400.0),
+                                    (pd.Timestamp('2012-01-02 00:00:00', offset='D'), 400.0),
+                                    (pd.Timestamp('2012-01-03 00:00:00', offset='D'), 400.0),
+                                    (pd.Timestamp('2012-01-04 00:00:00', offset='D'), 400.0),
+                                    (pd.Timestamp('2012-01-05 00:00:00', offset='D'), 10.0),
+                                    (pd.Timestamp('2012-01-06 00:00:00', offset='D'), 400.0),
+                                    (pd.Timestamp('2012-01-07 00:00:00', offset='D'), 400.0),
+                                    (pd.Timestamp('2012-01-08 00:00:00', offset='D'), 50.0),
+                                    (pd.Timestamp('2012-01-09 00:00:00', offset='D'), 50.0)]
+
+    sr = ts.sample_interval(sampling_period=timedelta(days=1), operation="min")
+    assert list(sr.iteritems()) == [(pd.Timestamp('2012-01-01 00:00:00', offset='D'), 400.0),
+                                    (pd.Timestamp('2012-01-02 00:00:00', offset='D'), 400.0),
+                                    (pd.Timestamp('2012-01-03 00:00:00', offset='D'), 400.0),
+                                    (pd.Timestamp('2012-01-04 00:00:00', offset='D'), 10.0),
+                                    (pd.Timestamp('2012-01-05 00:00:00', offset='D'), 10.0),
+                                    (pd.Timestamp('2012-01-06 00:00:00', offset='D'), 10.0),
+                                    (pd.Timestamp('2012-01-07 00:00:00', offset='D'), 50.0),
+                                    (pd.Timestamp('2012-01-08 00:00:00', offset='D'), 50.0),
+                                    (pd.Timestamp('2012-01-09 00:00:00', offset='D'), 50.0)]
