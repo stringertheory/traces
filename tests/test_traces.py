234312f52c761a37a4fd3f6a28f64670fde20543
Vlad Seghete <vlad.seghete@gmail.com>
2018-05-16 13:48:28 -0500

    added a test for the from_csv method

--- tests/test_traces.py
+++ tests/test_traces.py
@@ -1,7 +1,10 @@
 from datetime import datetime
 import pickle
+import nose
 
 from traces import TimeSeries
+import csv
+import os
 
 
 def test_init_data():
@@ -158,3 +161,42 @@ def test_pickle():
 
     unpickled[3] = unpickled[1]
     assert unpickled[3] is True
+
+
+def test_csv():
+    def time_parse(value):
+        return int(value)
+
+    def value_parse(value):
+        try:
+            return int(value)
+        except ValueError:
+            return None
+
+    filename = 'sample.csv'
+    with open(filename, 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['hour', 'value'])
+        writer.writerow(['10', '15'])
+        writer.writerow(['11', '34'])
+        writer.writerow(['12', '19'])
+        writer.writerow(['13', 'nan'])
+        writer.writerow(['14', '18'])
+        writer.writerow(['15', 'nan'])
+
+    ts = TimeSeries.from_csv(
+        filename,
+        time_column=0,
+        time_transform=time_parse,
+        value_column=1,
+        value_transform=value_parse,
+        default=None
+    )
+    os.remove(filename)
+
+    assert ts[9] is None
+    assert ts[20] is None
+    assert ts[13.5] is None
+
+    histogram = ts.distribution()
+    nose.tools.assert_almost_equal(histogram.mean(), (15 + 34 + 19 + 18) / 4.0)
