695eb51feb37ebcceff5ff02f7339f10968989da
stringertheory <mike.stringer.internet@gmail.com>
2019-12-29 16:12:07 -0600

    adding test to cover date parsing

--- tests/test_traces.py
+++ tests/test_traces.py
@@ -207,3 +207,21 @@ def test_csv():
 
     histogram = ts.distribution()
     nose.tools.assert_almost_equal(histogram.mean(), (15 + 34 + 19 + 18) / 4.0)
+
+    filename = 'sample.csv'
+    with open(filename, 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['hour', 'value'])
+        writer.writerow(['2000-01-01 10:00:00', '15'])
+        writer.writerow(['2000-01-01 11:00:00', '34'])
+        writer.writerow(['2000-01-01 12:00:00', '19'])
+        writer.writerow(['2000-01-01 13:00:00', 'nan'])
+        writer.writerow(['2000-01-01 14:00:00', '18'])
+        writer.writerow(['2000-01-01 15:00:00', 'nan'])
+
+    ts = TimeSeries.from_csv(filename)
+    os.remove(filename)
+
+    assert ts[datetime(2000, 1, 1, 9)] is None
+    assert ts[datetime(2000, 1, 1, 10, 30)] == '15'
+    assert ts[datetime(2000, 1, 1, 20)] == 'nan'
