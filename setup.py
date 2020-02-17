5938a0060c08d36ce902a61e2a48f3d5e2c6ee7d
stringertheory <mike.stringer.internet@gmail.com>
2020-02-17 15:48:40 -0600

    Bump version: 0.5.3 → 0.5.4

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='traces',
-    version='0.5.3',
+    version='0.5.4',
     description="A library for unevenly-spaced time series analysis.",
     long_description=long_description,
     long_description_content_type="text/markdown",
--- traces/__init__.py
+++ traces/__init__.py
@@ -9,4 +9,4 @@ from .utils import datetime_range
 
 __author__ = 'Mike Stringer'
 __email__ = 'mike.stringer@datascopeanalytics.com'
-__version__ = '0.5.3'
+__version__ = '0.5.4'
